86b0fe16ab118e72e5c6725e0233947811a6d04d
TE B 39 Jay Joshi <jayjoshi112711@gmail.com>
2022-05-28 15:15:09 +0530

    Deleted temp node on creation and Fixing RecyclerView

--- app/src/main/java/com/example/cms/ui/order/OrderFragment.java
+++ app/src/main/java/com/example/cms/ui/order/OrderFragment.java
@@ -3,6 +3,7 @@ package com.example.cms.ui.order;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -40,51 +41,78 @@ public class OrderFragment extends Fragment {
     private String phoneNumber;
 
     private TextView testText;
+    RecyclerView menuList;
+
+
+    public OrderFragment() throws InterruptedException {
+        Log.i("item","Constructor");
+//        initDataset();
+    }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        initDataset();
+        if(savedInstanceState==null) {
+//            initDataset();
+//            //Wait for HomeActivity to fetch phoneNumber
+//            try {
+//                TimeUnit.SECONDS.sleep(1);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+            phoneNumber = ((HomeActivity) getActivity()).getPhoneNumber();
+            //Deleting the temporary node created.
+            DatabaseReference deleter = FirebaseDatabase.getInstance("https://canteen-management-systems-20a8c.asia-southeast1.firebasedatabase.app/").getReference();
+            deleter.child("temp").child(phoneNumber).removeValue();
+        }
 
-//        //Wait for HomeActivity to fetch phoneNumber
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        //phoneNumber = ((HomeActivity) getActivity()).getPhoneNumber();
+        initDataset();
+        //Wait for HomeActivity to fetch phoneNumber
+        Log.i("item","Shown");
+        View root = inflater.inflate(R.layout.fragment_order, container, false);
+        menuList = (RecyclerView) root.findViewById(R.id.menuList);
+        testText = root.findViewById(R.id.testText);
+        menuList.setLayoutManager(new LinearLayoutManager(getActivity()));
+//        initDataset();
 //        try {
 //            TimeUnit.SECONDS.sleep(1);
 //        } catch (InterruptedException e) {
 //            e.printStackTrace();
 //        }
-        phoneNumber = ((HomeActivity) getActivity()).getPhoneNumber();
 
 
-    }
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-
-        //phoneNumber = ((HomeActivity) getActivity()).getPhoneNumber();
-        View root = inflater.inflate(R.layout.fragment_order, container, false);
-        RecyclerView menuList = (RecyclerView) root.findViewById(R.id.menuList);
-        testText = root.findViewById(R.id.testText);
-        menuList.setLayoutManager(new LinearLayoutManager(getActivity()));
-        initDataset();
-        try {
-            TimeUnit.SECONDS.sleep(1);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        MenuAdapter adapter = new MenuAdapter(menuItems, quantity, phoneNumber);
-        menuList.setAdapter(adapter);
         return root;
 
     }
 
+
+   @Override
+   public void onStop(){
+        super.onStop();
+//        menuItems.clear();
+//        quantity.clear();
+   }
     public void initDataset() {
+        Log.i("item","Init");
         dbref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Log.i("item","Init-loop");
                 for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
                     menuItems.add(dataSnapshot.getValue(MenuItem.class));
+                    Log.i("item",String.valueOf(menuItems.size()));
                     quantity.add(0);
                 }
+                MenuAdapter adapter = new MenuAdapter(menuItems, quantity, phoneNumber);
+                menuList.setAdapter(adapter);
+                Log.i("item","Init-end");
             }
 
             @Override
@@ -92,6 +120,7 @@ public class OrderFragment extends Fragment {
 
             }
         });
+        Log.i("item","End");
     }
 
     public void goToCart() {
